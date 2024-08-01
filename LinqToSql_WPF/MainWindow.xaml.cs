47cb17f381b75203933beec5cc332e3275415f8e
giolga <giolga.kumi@gmail.com>
2024-08-01 14:56:43 +0400

    Inserting objects into out database

--- LinqToSql_WPF/MainWindow.xaml.cs
+++ LinqToSql_WPF/MainWindow.xaml.cs
@@ -26,8 +26,47 @@ namespace LinqToSql_WPF
         {
             InitializeComponent();
 
-            string connectionString = ConfigurationManager.ConnectionStrings["LinqToSql.Properties.Settings.FirstDBConnectionString"].ConnectionString;
+            string connectionString = ConfigurationManager.ConnectionStrings["LinqToSql_WPF.Properties.Settings.FirstDBConnectionString"].ConnectionString;
             dataContext = new LinqToSqlDataClassesDataContext(connectionString);
+
+            //InsertUniversities();
+            InsertStudent();
+        }
+
+        public void InsertUniversities()
+        {
+            dataContext.ExecuteCommand("delete from University");
+
+            University cu = new University();
+            cu.Name = "CU";
+            dataContext.Universities.InsertOnSubmit(cu);
+
+            University btu = new University();
+            btu.Name = "BTU";
+            dataContext.Universities.InsertOnSubmit(btu);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+
+        public void InsertStudent()
+        {
+            University cu = dataContext.Universities.First(uni => uni.Name.Equals("CU"));
+            University btu = dataContext.Universities.First(uni => uni.Name.Equals("BTU"));
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student() { Name = "El Kumi", Gender = "Kacuri kaci", UniversityId = cu.Id});
+            students.Add(new Student() { Name = "Tony", Gender = "Male", University = cu });
+            students.Add(new Student() { Name = "Mari", Gender = "Female", University = cu });
+            students.Add(new Student() { Name = "Lucy", Gender = "Feale", University = btu });
+
+            dataContext.Students.InsertAllOnSubmit(students);
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
+
         }
     }
 }
---
+++ README.md
@@ -0,0 +1,1 @@
+# Linq-to-SQL
